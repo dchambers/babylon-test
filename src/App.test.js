7e9de1f06e6d1a172e01a8f77fed231ff5f15db5
Dominic Chambers <dchambers@sapient.com>
2017-05-11 16:46:11 +0100

    Early commit of checkbox tests
    30 minutes is up, so committing early...

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
 import App from './App';
+import Checkbox from './Checkbox';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+
+it('has two checkboxes', () => {
+  const app = shallow(<App />);
+  expect(app.find(Checkbox)).to.have.length(2);
+});
+
+it('has Internet checked by default', () => {
+  const app = shallow(<App />);
+  // ...
 });
